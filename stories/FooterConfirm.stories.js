f6c0983809cf169ab9eb8b4f066d4ff4982f8b84
ftandas <ftandas@my.bcit.ca>
2021-04-17 19:17:29 -0700

    added comps to storybook

---
+++ stories/FooterConfirm.stories.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import FooterBackNextUI from '../comps/Bottom/FooterBackNext';
+import BackUI from '../comps/Bottom/Back';
+import BackButtonUI from '../comps/Bottom/BackButton';
+import ConfirmButtonUI from '../comps/Bottom/ConfirmButton';
+import FooterConfirmUI from '../comps/Bottom/FooterConfirm';
+import NextButtonUI from '../comps/Bottom/NextButton';
+
+export default {
+    title:"Bottom Footers",
+    component: <FooterConfirmUI />
+}
+
+export const BackUI = () => <FooterConfirmUI />
+
+export const ConfirmButtonUI = () => <FooterConfirmUI />
+
+
+ 
---
+++ stories/InfoPopup.stories.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import InfoPopupUI from '../comps/InfoPopup/InfoPopup';
+import PopBackgroundUI from '../comps/InfoPopup/PopBackground';
+import CloseButtonUI from '../comps/InfoPopup/CloseButton';
+import ContentUI from '../comps/InfoPopup/Content';
+import PopUpUI from '../comps/InfoPopup/PopUp';
+import InfoButtonUI from '../comps/InfoPopup/InfoButton';
+
+export default {
+    title:"Info Pop-Up",
+    component:<InfoPopupUI />
+}
+
+export const InfoButtonUI = () => <InfoPopupUI />
+
+export const PopUpUI = () => <InfoPopupUI />
