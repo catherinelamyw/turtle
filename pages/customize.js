a9668e60a73ac16c37aa6de12515e2ec75113d1e
catherinelamyw <clam166@my.bcit.ca>
2021-05-18 21:55:35 -0700

    finished changing color interaction on customize page

--- pages/customize.js
+++ pages/customize.js
@@ -4,10 +4,25 @@ import ColorUI from '../comps/Color'
 import HeadingUI from '../comps/Heading'
 import TextboxUI from '../comps/Textbox'
 import WhiteTextUI from '../comps/WhiteText'
+import React, {useState} from 'react'
 import {MdArrowBack, MdArrowForward} from 'react-icons/md';
 import {HiMenu} from 'react-icons/hi'
 
 export default function Customize() {
+  const [imgcolor, setImgcolor] = useState("/drawings/_grn-start.png")
+
+  const HandleGreen = () => {
+    setImgcolor("/drawings/_grn-start.png");
+  }
+  
+  const HandleBrown = () => {
+    setImgcolor("/drawings/_brn-start.png");
+  }
+  
+  const HandleBlack = () => {
+    setImgcolor("/drawings/_blk-start.png");
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.topmargin}>
@@ -15,14 +30,16 @@ export default function Customize() {
       </div>
       <HeadingUI text='Customize'/>
       <div className={styles.graphics}>
-        <MdArrowBack color='white' size='50px' title='left arrow' className={styles.symbols}/>
-        <img className={styles.image} src='/drawings/_grn-start.png' />
-        <MdArrowForward color='white' size='50px' title='right arrow' className={styles.symbols}/>
+        {/* <MdArrowBack color='white' size='50px' title='left arrow' className={styles.symbols}/> */}
+        <img className={styles.image} src={imgcolor} />
+        {/* <MdArrowForward color='white' size='50px' title='right arrow' className={styles.symbols}/> */}
       </div>
       <div className={styles.colors}>
-        <ColorUI/>
-        <ColorUI color='#8C6239'/>
-        <ColorUI color='#272C3B'/>
+        <ColorUI
+          onGrnClick={HandleGreen}
+          onBrnClick={HandleBrown}
+          onBlkClick={HandleBlack}
+        />
       </div>
       <div className={styles.row}>
         <WhiteTextUI text='NAME'/>
